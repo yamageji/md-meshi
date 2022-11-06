99efe913f68a43c772ba843ee85f6727db6b9f71
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2022-11-06 21:21:42 +0900

    fix: router.options

---
+++ app/router.options.ts
@@ -0,0 +1,60 @@
+import { nextTick } from 'vue';
+// import { useNuxtApp } from '#app';
+export default {
+  scrollBehavior(to, from, savedPosition) {
+    const nuxtApp = useNuxtApp();
+    let position = savedPosition || void 0;
+    if (
+      !position &&
+      from &&
+      to &&
+      to.meta.scrollToTop !== false &&
+      _isDifferentRoute(from, to)
+    ) {
+      position = { left: 0, top: 0 };
+    }
+    if (to.path === from.path) {
+      if (from.hash && !to.hash) {
+        return { left: 0, top: 0 };
+      }
+      if (to.hash) {
+        return { el: to.hash, top: _getHashElementScrollMarginTop(to.hash) };
+      }
+    }
+    const hasTransition =
+      to.meta.pageTransition !== false && from.meta.pageTransition !== false;
+    const hookToWait = 'page:finish';
+    return new Promise((resolve) => {
+      nuxtApp.hooks.hookOnce(hookToWait, async () => {
+        await nextTick();
+        if (to.hash) {
+          position = {
+            el: to.hash,
+            top: _getHashElementScrollMarginTop(to.hash),
+          };
+        }
+        resolve(position);
+      });
+    });
+  },
+};
+function _getHashElementScrollMarginTop(selector) {
+  const elem = document.querySelector(selector);
+  if (elem) {
+    return parseFloat(getComputedStyle(elem).scrollMarginTop);
+  }
+  return 0;
+}
+function _isDifferentRoute(a, b) {
+  const samePageComponent = a.matched[0] === b.matched[0];
+  if (!samePageComponent) {
+    return true;
+  }
+  if (
+    samePageComponent &&
+    JSON.stringify(a.params) !== JSON.stringify(b.params)
+  ) {
+    return true;
+  }
+  return false;
+}
